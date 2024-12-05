9554411c231a510e16b22a75dc86f5060bb19598
$ LaBiOmicS <labiomics@bioinformatica.com.br>
2024-12-05 12:05:35 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,19 @@
-// Configurações padrão do modelo 3D
 export const config = {
     cubeSize: 1,
     gap: 0.2,
     baseHeights: {
-        A: 3,
-        T: 4,
-        C: 5,
-        G: 6,
-        N: 2
+        'A': 3,
+        'T': 4,
+        'C': 5,
+        'G': 6,
+        'N': 2
+    },
+
+    getConfig() {
+        return {
+            cubeSize: this.cubeSize,
+            gap: this.gap,
+            baseHeights: this.baseHeights
+        };
     }
 };
