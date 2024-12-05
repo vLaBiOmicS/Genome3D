7fef3ecd48d810a08a5bc4aa3b1e3302362fa228
$ LaBiOmicS <labiomics@bioinformatica.com.br>
2024-12-05 11:52:45 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// Configurações padrão do modelo 3D
+export const config = {
+    cubeSize: 1,
+    gap: 0.2,
+    baseHeights: {
+        A: 3,
+        T: 4,
+        C: 5,
+        G: 6,
+        N: 2
+    }
+};
